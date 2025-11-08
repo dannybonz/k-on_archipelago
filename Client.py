e3868fbf93f89834e54ff658777949fc574f8083
Danny <bonzorio1@gmail.com>
2025-11-08 11:24:37 +0000

    Increase version number

--- Client.py
+++ Client.py
@@ -21,7 +21,7 @@ class KONCommandProcessor(ClientCommandProcessor):
             log_tokens(self.ctx)
 
 class KONContext(CommonContext):
-    client_version: str = "v1.1.1"
+    client_version: str = "v1.1.2"
 
     game: str = "K-On! After School Live!!"
 
